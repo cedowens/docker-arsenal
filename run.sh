8e522c32ba380ffe82135640b7d8edd04feaec71
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-06-16 21:46:47 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,15 +4,10 @@ git clone https://github.com/cedowens/MacC2
 git clone https://github.com/its-a-feature/Mythic
 git clone https://github.com/FSecureLABS/Jamf-Attack-Toolkit
 git clone https://github.com/initstring/cloud_enum
-git clone https://github.com/0xsha/CloudBrute
-git clone https://github.com/zricethezav/gitleaks
-git clone https://github.com/kgretzky/evilginx2
 git clone https://github.com/D00MFist/PersistentJXA
 git clone https://github.com/RhinoSecurityLabs/pacu
 git clone https://github.com/cedowens/aws_key_triage_tool
 git clone https://github.com/cedowens/keygrabber
-git clone https://github.com/grines/scour
-git clone https://github.com/grines/hashiscan
 git clone https://github.com/cedowens/JenkinsHunter
 
 docker build . --no-cache -t mac-arsenal
